efbbf3bb90dbeaa2093aab2d807ae13b72faa4ab
AungKyawSoeOo <iamaprogrammer123@gmail.com>
2023-02-27 21:27:28 +0630

    lifecycle

---
+++ lifecycle-hook/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+//  number:number=12345; {{number}} in app.component.html
+//  name:string="ko ko"  {{name}} in app.component.html
+}
