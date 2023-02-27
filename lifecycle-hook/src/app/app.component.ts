b14705b4d9bb5906e4651170305c36f4b379fee1
AungKyawSoeOo <iamaprogrammer123@gmail.com>
2023-02-27 22:10:45 +0630

    a

--- lifecycle-hook/src/app/app.component.ts
+++ lifecycle-hook/src/app/app.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
 //  number:number=12345; {{number}} in app.component.html
 //  name:string="ko ko"  {{name}} in app.component.html
+  private number:number =12345;
+  get counter(){
+    return this.number
+  }
+  
+  set counter(value){
+    this.number=value;
+  }
+
+  increment(){
+    this.counter++;
+  }
+  decrement(){
+    this.counter--;
+  }
 }
