ff5fbc47a579cb5e5d8b1d9510cf74a012eea75b
AungKyawSoeOo <iamaprogrammer123@gmail.com>
2023-03-04 20:25:25 +0630

    data passing

---
+++ TodoListAndDataPassing(CTP,PTC)/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TodoList';
+  tasks:any[]=[];
+  users=[
+    {name:'aung',email:'aung@gmail.com'},
+    {name:'john',email:'john@gmail.com'}
+  ]
+  parent='I came from parent';
+  addTask(item:string){
+    console.log(item)
+    this.tasks.push({id:this.tasks.length,name:item});
+  };
+  removeTask(id:number){
+    this.tasks=this.tasks.filter(task=>task.id!==id);
+  }
+}
