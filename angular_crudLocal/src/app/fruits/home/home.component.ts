31e3b49ae042e8cea283aa6656004bd1f9bcbf90
AungKyawSoeOo <iamaprogrammer123@gmail.com>
2023-03-06 20:56:55 +0630

    angular crude

---
+++ angular_crudLocal/src/app/fruits/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Fruits } from '../fruits';
+import { FruitsService } from '../fruits.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  allFruits: Fruits[] = [];
+  constructor(private fruitService: FruitsService) { }
+
+  ngOnInit(): void {
+    this.get();
+  }
+
+  get() {
+    this.fruitService.get().subscribe((data) => {
+      this.allFruits = data;
+    });
+  }
+
+
+  delete(item: any) {
+    let result = confirm(`Are you sure to delete '${item.name}'`);
+    if (result) {
+      this.fruitService.delete(item.id).subscribe({
+        next: (data) => {
+          this.allFruits = this.allFruits.filter(fruit => fruit.id != item.id);
+        },
+      });
+    }
+  }
+}
